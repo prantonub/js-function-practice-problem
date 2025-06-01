2639dc8b4d05a35da26ee033dea2e8141093d9b5
prantonub <prantonub.cse23@gmail.com>
2025-06-02 02:01:08 +0600

    task added

--- task3.js
+++ task3.js
@@ -1,3 +1,21 @@
 /**### Task-3
 Write a function called make_avg() which will take an array of integers 
 and the size of that array and return the average of those values. */
+function make_avg(numbers, size) {
+    if (numbers.length !== size) {
+        console.log("Error: Array size doesn't match the given size.");
+        return;
+    }
+    
+    let sum = 0;
+    for (let i = 0; i < size; i++) {
+        sum = sum + numbers[i];
+    }
+
+    const average = sum / size;
+    return average;
+}
+
+const numbers = [10, 20, 30, 40, 50];
+const size = 5;
+console.log("Average:", make_avg(numbers, size)); // Output: 30
--- task4.js
+++ task4.js
@@ -3,3 +3,21 @@ Write a function called count_zero() which will take a binary string
 (Binary string is a string which is consist of only 0 and 1) as parameter and 
 count how many 0’s are there in that string.
  */
+/* Task-4  
+Write a function called count_zero() which will take a binary string 
+(Binary string is a string which consists of only 0 and 1) as a parameter and 
+count how many 0’s are there in that string.
+*/
+
+function count_zero(binaryStr) {
+    let count = 0;
+    for (let i = 0; i < binaryStr.length; i++) {
+        if (binaryStr[i] === '0') {
+            count++;
+        }
+    }
+    return count;
+}
+
+const binary = "1010010100";
+console.log("Number of 0's:", count_zero(binary)); // Output: 6
