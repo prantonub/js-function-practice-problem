cc205751302d8793e515792de05daa62f68adcf4
prantonub <prantonub.cse23@gmail.com>
2025-06-03 16:02:08 +0600

    task added

---
+++ task7.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function called square() which takes a number as a parameter, 
+ * squares it (multiply by itself), and returns the result.
+ */
+function square(num) {
+    return num * num;
+  }
+  
+  console.log(square(5)); // Output: 25
+  console.log(square(9)); // Output: 81
+  
