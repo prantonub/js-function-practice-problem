533e687077e054825db6d0dc64a3c8f032bcf171
prantonub <prantonub.cse23@gmail.com>
2025-06-03 16:17:04 +0600

    task added

---
+++ task11.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function minutes_to_seconds() that takes minutes as input and 
+ * returns the value in seconds.
+ */
+function minutes_to_seconds(minutes) {
+    return minutes * 60;
+  }
+  
+console.log(minutes_to_seconds(1)); // 60
+console.log(minutes_to_seconds(5)); // 300
+  
