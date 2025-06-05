d5362c93c77fea0a566841860f0f8a0ad3eeaf22
prantonub <prantonub.cse23@gmail.com>
2025-06-05 23:38:47 +0600

    task added

---
+++ task12.js
@@ -0,0 +1,9 @@
+// ### Task-12
+// Takes a string and returns it in uppercase.
+
+function toUpperCaseString(str) {
+    return str.toUpperCase();
+}
+  
+console.log(toUpperCaseString("hello world")); // Output: "HELLO WORLD"
+  
