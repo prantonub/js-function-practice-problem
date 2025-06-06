98c70a499016ac85ff1eb2652ea31dc45f5f7007
prantonub <prantonub.cse23@gmail.com>
2025-06-06 23:56:57 +0600

    task added

--- task20.js
+++ task20.js
@@ -1,11 +1,8 @@
-### Task-20
-Multiply the input number by 10 and return the result.
-
+// ### Task-20
+// Multiply the input number by 10 and return the result.
 function multiplyByTen(num) {
     return num * 10;
-  }
-  
-  // Example usage:
-  console.log(multiplyByTen(5));   // Output: 50
-  console.log(multiplyByTen(-3));  // Output: -30
+}
+console.log(multiplyByTen(5));   // Output: 50
+console.log(multiplyByTen(-3));  // Output: -30
   
