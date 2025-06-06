895ab487831d1b7e5670fce1191a0b2020651745
prantonub <prantonub.cse23@gmail.com>
2025-06-06 23:47:32 +0600

    task added

---
+++ task16.js
@@ -0,0 +1,9 @@
+// ### Task-16
+// Returns True if the number is negative, else False.
+function isNegative(number) {
+    return number < 0;
+}
+  
+console.log(isNegative(-5)); // Output: true
+console.log(isNegative(3));  // Output: false
+  
