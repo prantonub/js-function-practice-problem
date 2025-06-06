d78b692f042686c5105f1f64505f4a7c32debbdb
prantonub <prantonub.cse23@gmail.com>
2025-06-06 23:54:27 +0600

    task added

---
+++ task19.js
@@ -0,0 +1,9 @@
+// ### Task-19
+// Returns the smaller of the two numbers.
+function smallerNumber(a, b) {
+    return a < b ? a : b;
+}
+
+console.log(smallerNumber(5, 8));  
+console.log(smallerNumber(10, 3)); 
+  
