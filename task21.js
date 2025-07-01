14243b6d6f2609dbb6e3e0d85f46d478cc4114f2
prantonub <prantonub.cse23@gmail.com>
2025-07-02 01:52:37 +0600

    task added

--- task21.js
+++ task21.js
@@ -5,3 +5,10 @@ function addSeven(num) {
 }
 console.log(addSeven(3));   // Output: 10
 console.log(addSeven(-4));  // Output: 3
+
+//Divide the input number by 2 and return the result.
+function divideByTwo(num) {
+    return num / 2;
+}
+console.log(divideByTwo(10));  // Output: 5
+console.log(divideByTwo(7));   // Output: 3.5
